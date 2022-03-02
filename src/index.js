2398a76a40fbcf0a4502196dd2c4a7603039e80e
auzanzulhazmi0509 <zulhazmiauzan@gmail.com>
2022-03-02 09:52:59 +0700

    HelloComponent

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Auzan from './Auzan';
+// import Auzan from './Auzan';
+
+import HelloComponent from './HelloComponent';
 import reportWebVitals from './reportWebVitals';
 
 // function HelloComponent () {
@@ -25,7 +27,9 @@ import reportWebVitals from './reportWebVitals';
 //   }
 // }
 
-ReactDOM.render(<Auzan />, document.getElementById('root'));
+// ReactDOM.render(<Auzan />, document.getElementById('root'));
+
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
