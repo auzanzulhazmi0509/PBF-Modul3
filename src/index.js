415904e0bdec55bab27201d76f392919bcc65192
auzanzulhazmi0509 <zulhazmiauzan@gmail.com>
2022-03-02 09:18:32 +0700

    HelloComponent

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ function HelloComponent () {
   return HelloComponent
 }
 
-class StateFullComponent extends React.Component{
-  render(){
-    return <p>StateFullComponent</p>
-  }
-}
+// class StateFullComponent extends React.Component{
+//   render(){
+//     return <p>StateFullComponent</p>
+//   }
+// }
 
-ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
