f5becd6329ad4277442bf2f202e5bb3c59c3eb8d
auzanzulhazmi0509 <zulhazmiauzan@gmail.com>
2022-03-02 10:26:56 +0700

    Update Props

--- src/index.js
+++ src/index.js
@@ -27,9 +27,11 @@ import reportWebVitals from './reportWebVitals';
 //   }
 // }
 
+ReactDOM.render(<App />, document.getElementById('root'));
+
 // ReactDOM.render(<Auzan />, document.getElementById('root'));
 
-ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
